0853ee344cc39bdfb157ba024b5eedd23914aa11
noraj <alexandre.zanni@europe.com>
2020-07-25 17:12:35 +0200

    modify the generator for OSWE

--- generate.rb
+++ generate.rb
@@ -2,22 +2,30 @@
 
 templates = [
   {
+    exam: 'OSCP',
     name: 'whoisflynn improved template v3.2',
     path: 'src/OSCP-exam-report-template_whoisflynn_v3.2.md'
   },
   {
-    name: 'Official Offensive Security Template v1',
+    exam: 'OSCP',
+    name: 'official Offensive Security template v1',
     path: 'src/OSCP-exam-report-template_OS_v1.md'
+  },
+  {
+    exam: 'OSWE',
+    name: 'official Offensive Security template v1',
+    path: 'src/OSWE-exam-report-template_OS_v1.md'
   }
 ]
 
 # Choose template
 puts 'Choose a template:'
 templates.each_with_index do |t,i|
-  puts "#{i}. #{t[:name]}"
+  puts "#{i}. [#{t[:exam]}] #{t[:name]}"
 end
 choice = gets.chomp
 src = templates[choice.to_i][:path]
+exam = templates[choice.to_i][:exam]
 
 # Enter your OS id
 puts 'Enter your OS id'
@@ -33,7 +41,7 @@ puts style
 
 # Generating report
 puts 'Generating report...'
-pdf = "output/OSCP-#{osid}-Exam-Report.pdf"
+pdf = "output/#{exam}-#{osid}-Exam-Report.pdf"
 %x(pandoc #{src} -o #{pdf} \
   --from markdown+yaml_metadata_block+raw_html \
   --template eisvogel \
@@ -46,7 +54,7 @@ pdf = "output/OSCP-#{osid}-Exam-Report.pdf"
 
 # Generating archive
 puts 'Generating archive...'
-%x(7z a output/OSCP-#{osid}-Exam-Report.7z \
+%x(7z a output/#{exam}-#{osid}-Exam-Report.7z \
   -p#{osid} \
   #{File.expand_path(pdf)}
 )
