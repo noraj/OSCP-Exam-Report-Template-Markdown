7e04665083f38221e7724dd0d1b7aa2555594d35
xl-sec <xl-sec@users.noreply.github.com>
2021-10-05 21:55:08 +0200

    Option to view PDF after generating and whether to generate archive or not

--- generate.rb
+++ generate.rb
@@ -6,6 +6,8 @@ options = {
   # template: 0,
   # osid: 'OS-####',
   # style: 'breezedark',
+  # view: true,
+  # archive: true,
   # extra_file: false
 }
 
@@ -13,9 +15,11 @@ require 'optparse'
 OptionParser.new do |opts|
   opts.banner = "Usage: generate.rb [options]"
   opts.on("-t", "--template ID", "Template ID") { |o| options[:template] = o }
-  opts.on("-i", "--os-id ID", "OS-ID") { |o| options[:osid] = o }
-  opts.on("-s", "--style STYLE", "Code highlight style (or 'd' for default)") { |o| options[:style] = o != 'd' ? o : false  }
-  opts.on("-e", "--extra FILE", "Extra file to include in archive (or 'n' for none)") { |o| options[:extra_file] = o != 'n' ? o : false }
+  opts.on("-i", "--os-id ID",    "Offensive Security student ID (OS-ID)") { |o| options[:osid] = o }
+  opts.on("-s", "--style STYLE", "Code highlight style (or 'd' for default which is breezedark)") { |o| options[:style] = o != 'd' ? o : false  }
+  opts.on("-v", "--view Y/N",    "View PDF after generating with the OS default PDF viewer") { |o| options[:view] = o.downcase == 'y' }
+  opts.on("-a", "--archive Y/N", "Create an archive") { |o| options[:archive] = o.downcase == 'y' }
+  opts.on("-e", "--extra FILE",  "Extra file(s) to include in archive (or 'n' for none)") { |o| options[:extra_file] = o != 'n' ? o : false }
 end.parse!
 
 templates = [
@@ -80,18 +84,19 @@ puts "---[ OffSec Exam Report Generator ]---"
 
 # Choose template
 if options.key?(:template) and options[:template].to_i.between?(0, templates.length())
-  puts "Chosen template: #{templates[options[:template].to_i][:name]}"
-  src = templates[options[:template].to_i][:path]
-  exam = templates[options[:template].to_i][:exam]
+  template = options[:template].to_i
+  puts "Chosen template: #{templates[template][:name]}"
+  src = templates[template][:path]
+  exam = templates[template][:exam]
 else
   puts "Available templates;"
   templates.each_with_index do |t,i|
     puts "#{i}. [#{t[:exam]}] #{t[:name]}"
   end
   print 'Choose a template: '
-  choice = gets.chomp
-  src = templates[choice.to_i][:path]
-  exam = templates[choice.to_i][:exam]
+  template = gets.chomp.to_i
+  src = templates[template][:path]
+  exam = templates[template][:exam]
 end
 
 # Enter your OS id
@@ -107,11 +112,14 @@ end
 style = 'breezedark'
 styles = %x(pandoc --list-highlight-styles).split("\n")
 if options.key?(:style) and (!options[:style] or options[:style].downcase == 'd')
+  # Default style according to argument
   puts "Style: #{style}"
 elsif options.key?(:style) and options[:style] == options[:style].to_i.to_s and options[:style].to_i.between?(0, styles.length())
+  # Argument set style by integer
   style = styles[options[:style].to_i]
   puts "Style: #{style}"
 elsif options.key?(:style) and styles.include?(options[:style])
+  # Argument set style by string
   style = options[:style]
   puts "Style: #{style}"
 else
@@ -124,24 +132,47 @@ else
   style = styles[choice.to_i] unless choice.empty?
 end
 
-# Optional lab report
-extra_file = false
-if options.key?(:extra_file)
-  if !options[:extra_file] or options[:extra_file].downcase == 'n'
-    nil
-  else
-    extra_file = options[:extra_file]
-    puts "Extra file: #{extra_file}"
-  end
+view = true
+if options.key?(:view)
+  view = options[:view].to_s.downcase == "true"
 else
-  print 'Do you want to add an external lab report? [y/N]: '
+  print 'Do you want to view the report after generating? [Y/n]: '
   choice = gets.chomp
-  if choice.downcase == 'y'
-    print 'Write the path of your lab PDF: '
-    extra_file = gets.chomp
+  if choice.downcase == 'n'
+    view = false
   end
 end
 
+archive = true
+if options.key?(:archive)
+  archive = options[:archive].to_s.downcase == "true"
+else
+  print 'Do you want to archive the report? [Y/n]: '
+  choice = gets.chomp
+  if choice.downcase == 'n'
+    archive = false
+  end
+end
+
+extra_file = false
+if archive
+  # Optional lab report
+  if options.key?(:extra_file)
+    if !options[:extra_file] or options[:extra_file].downcase == 'n'
+      nil
+    else
+      extra_file = options[:extra_file]
+      puts "Extra file(s): #{extra_file}"
+    end
+  else
+    print 'Do you want to add extra files to the archive? [y/N]: '
+    choice = gets.chomp
+    if choice.downcase == 'y'
+      print 'Write the path to the file(s): '
+      extra_file = gets.chomp
+    end
+  end
+end
 
 # We assume that the template and output directory is always relative to the
 # script location and not where you call it, this makes it easier to set up an
@@ -167,37 +198,52 @@ print 'Generating report...'
 )
 puts "done"
 
-# Removing old archive
-print 'Cleaning archive...'
-%x(rm \
-  #{archive_path}
-)
-puts "done"
-
-# Generating archive
-print 'Generating archive...'
-%x(7z a #{archive_path} \
-  #{pdf_path}
-)
-puts "done"
-
-# Updating archive
-if extra_file
-  print 'Updating archive...'
-  %x(7z a #{archive_path} \
-    #{File.expand_path(extra_file, File.dirname(__FILE__))}
-  )
+if view
+  # Opening viewer in new process
+  print 'Viewing PDF...'
+  viewer = fork do
+    exec "open #{pdf_path}"
+  end
+  Process.detach(viewer)
   puts "done"
 end
 
-# Calculate MD5
-print 'Calculating MD5...'
-md5 = %x(md5sum \
-  #{archive_path} \
-  | cut -d ' ' -f 1
-)
-puts "done"
+if archive
+  # Removing old archive
+  print 'Cleaning archive...'
+  `rm \
+    #{archive_path} \
+    2>/dev/null
+  `
+  puts "done"
 
-puts
-puts "Generated archive #{filename}.7z"
-puts "with MD5 #{md5}"
+  # Generating archive
+  print 'Generating archive...'
+  %x(7z a #{archive_path} \
+    #{pdf_path}
+  )
+  puts "done"
+
+  # Updating archive
+  if extra_file
+    # For additional files we're assuming relative paths
+    # else we need to split and expand individually
+    print 'Updating archive...'
+    %x(7z a #{archive_path} \
+      #{extra_file}
+    )
+    puts "done"
+  end
+
+  # Calculate MD5
+  print 'Calculating MD5...'
+  md5 = %x(md5sum \
+    #{archive_path} \
+    | cut -d ' ' -f 1
+  )
+  puts "done"
+
+  puts
+  puts "Generated archive #{filename}.7z"
+  puts "with MD5 #{md5}"
+end
